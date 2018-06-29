6b41491587e30a2e846b445038db51a539606693
babambabambi <babambabambi@gmail.com>
2018-06-29 17:09:43 +0900

    javascript 연습

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,12 +3,32 @@ class CommentsController < ApplicationController
   def create
     @comment = Post.find(params[:post_id]).comments.new(comment_params)
     @comment.user_id = current_user.id
-    @comment.save
-    redirect_to :back
+    if @comment.save
+      respond_to do |format|
+        format.html {redirect_to :back}
+        format.js {render 'create_temp'}//action명과 다른 파일로 렌더링 하기 위해 명시
+      end
+    else
+      redirect_to :back
+    end
+  end
+
+  def destroy
+    @comment=Comment.find(params[:comment_id])
+    @comment.destroy
+    respond_to do |format|
+      format.html {redirect_to :back}
+      format.js {}
+    end
   end
 
   private
   def comment_params
     params.permit(:content)
   end
+
+
+
+
+
 end
